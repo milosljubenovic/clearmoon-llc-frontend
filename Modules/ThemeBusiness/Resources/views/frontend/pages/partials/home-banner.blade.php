c89193a8628b74b4202da436dda26958c789c2cb
Your Name <you@example.com>
2024-08-14 15:12:39 +0200

    first commit

---
+++ Modules/ThemeBusiness/Resources/views/frontend/pages/partials/home-banner.blade.php
@@ -0,0 +1,51 @@
+<section class="section section-with-shape-divider section-parallax bg-tertiary border-0 m-0" data-plugin-parallax="" data-plugin-options="{&#39;speed&#39;: 1.5, &#39;parallaxHeight&#39;: &#39;110%&#39;, &#39;fadeIn&#39;: true}" data-image-src="{{ asset('public/modules/theme-business/images/common/background-4.jpg') }}" style="position: relative; overflow: hidden; z-index: 1;">
+    <div class="container position-relative z-index-2 pb-5 my-5">
+        <div class="row justify-content-between pb-5 mb-5">
+            <div class="col-md-6 text-start pe-lg-4 pb-5 mb-5">
+                <h1 class="text-color-light font-weight-medium line-height-4 text-10 mb-5 appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="300" style="animation-delay: 300ms;">
+                    High Quality <span class="font-weight-extra-bold custom-highlight-1 ws-nowrap p-1 customHighlightAnim customHighlightAnimDelay">OnDemand</span> IT Services
+                </h1>
+                <sub class="text-color-light font-weight-medium text-4 mb-5 appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="500" style="animation-delay: 500ms;">
+                    We provide high quality and cost effective services,<br>to meet your IT needs. From zero to Hero, within just days!
+                </sub>
+            </div>
+            <div class="col-md-5 d-flex justify-content-end">
+                <div class="card border-0 custom-border-radius-1 box-shadow-1 appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="900" style="animation-delay: 900ms; z-index: 3;">
+                    <div class="card-body p-5">
+                        <h2 class="text-color-secondary font-weight-bold text-7 line-height-1 mb-1">
+                            Request a Service
+                        </h2>
+                        <p class="custom-font-secondary text-4 pb-1 mb-4">
+                            Do you need any of the services provided by us? Follow the Steps:
+                        </p>
+                        <ul>
+                            <li>Create a Booking Request</li>
+                            <li>Wait for Authority Call</li>
+                            <li>& Get the Best Experience of Servicing in <b>{{ config('app.name') }}</b></li>
+                        </ul>
+                        <a href="{{ route('demo.business.booking_request.create') }}" class="btn btn-primary btn-modern font-weight-bold text-3 px-5 py-3" style="z-index: 4;">
+                            Book a Service Now
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="parallax-background fadeIn animated" style="background-image: url('{{ asset('public/modules/theme-business/images/common/background-1.jpg') }}'); background-size: cover; position: absolute; top: 0px; right: 0px; width: 50%; height: 110%; transform: translate3d(0px, 1699.71px, 0px); background-position-x: 50%; z-index: 0;">
+    </div>
+    <div class="shape-divider shape-divider-bottom" style="height: 576px; z-index: 1;">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1920 576" preserveAspectRatio="xMinYMin">
+            <path class="custom-svg-fill-color-tertiary" d="M-12,66c13.35,40.03,28.35,72.62,41,97c12.78,24.63,30.56,58.44,62,97c19.57,24,36.66,40.31,49,52
+                        c41.02,38.87,77.1,62.82,85,68c53.78,35.23,100.92,51.3,148,67c87.6,29.21,158.49,37.98,201,43c63.9,7.55,112.08,7.78,160,8
+                        c16.52,0.08,84.41,0.17,181-8c98.88-8.36,172.43-20.7,233-31c42.88-7.29,141.31-24.85,267-56c127.06-31.5,220.66-61.69,296-86
+                        c89.27-28.8,159.58-54.68,174-60c16.6-6.13,30.39-11.34,40-15c0,113.33,0,226.67,0,340c-644.33,0-1288.67,0-1933,0
+                        C-9.33,410-10.67,238-12,66z"></path>
+            <path fill="#FFF" d="M-5,3c1.21,5.32,2.98,13.29,5,23c4.38,21.08,6.71,34.95,8,42c7.08,38.6,19.7,71.54,26,88
+                        c8.52,22.26,16.34,38.54,19,44c5.4,11.07,16.38,32.61,33,58c28.88,44.11,57.48,73.41,64,80c6.89,6.97,38.98,38.97,91,72
+                        c57.09,36.25,106.11,53.37,143,66c66.98,22.93,119.93,31.44,162,38c81,12.63,143.81,13.53,189,14c62.45,0.65,109.33-2.68,156-6
+                        c35.74-2.54,89.63-7.16,155-16c68.17-9.22,117.88-18.83,170-29c110.32-21.52,194.58-42.54,224-50c131.43-33.34,227.73-64.9,286-84
+                        c93.88-30.77,155.18-54.57,184-66c6.29-2.49,11.48-4.58,15-6c0,102.67,0,205.33,0,308c-643.33,0-1286.67,0-1930,0C-5,387-5,195-5,3z
+                        "></path>
+        </svg>
+    </div>
+</section>
